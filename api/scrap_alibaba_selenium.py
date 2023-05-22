4fb266b1df4f1a7865aa472cade970a38f917655
Huzaifa Abid <huzaifaa926@gmail.com>
2023-05-22 16:56:01 +0500

    finished partial scrapping, started v3

---
+++ api/scrap_alibaba_selenium.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import requests
+import urllib.parse
+import re
+
+LIMIT = 10
+options = Options()
+options.add_argument("--headless")
+
+def normalize_url(url):
+    if url.startswith('//'):
+        url = url[2:]
+    return url.lstrip('/')
+
+
+def scrap(keyword):
+    keyword = urllib.parse.quote(keyword)
+    driver = webdriver.Chrome(options=options)
+    driver.get(f"https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText={keyword}&viewtype=G&page=1")
+    html = driver.page_source
+    driver.close()
+
+    soup = BeautifulSoup(html, 'lxml')
+    product_lists = soup.find('div', {'class': 'app-organic-search__list'}).findChildren("div" , recursive=False)
+    data = []
+    for product in product_lists[:LIMIT]:
+        temp = {}
+        if product.find('h2'):
+            link = product.find('h2').find('a')['href']
+            link = 'https://' + normalize_url(link)
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+            response=requests.get(link, headers=headers)
+            soup=BeautifulSoup(response.text,'lxml')
+            title = soup.find('div', {'class':'product-title'})
+            price = soup.find('div', {'class':'product-price'}).find_all(class_='price')
+            quality = soup.find('div', {'class':'product-price'}).find_all(class_='quality')
+            UoM = soup.find('div', {'class':'product-price'}).find_all(class_='unit')
+
+            if title:
+                title = title.text.strip()
+            if price:
+                price = price[0].text.strip()
+                match = re.search(r'\d+(?:,\d+)?(?:\.\d+)?', price)
+                if match:
+                    value = match.group()
+            if quality:
+                quality = quality[0].text.strip()
+            if UoM:
+                UoM = UoM[0].text.strip()
+            
+            temp['title'] = title
+            
+            
+            temp['price'] = float(value)
+            temp['link'] = link
+            temp['quality'] = quality
+            temp['uom'] = UoM
+            temp['currency'] = "USD"
+
+            data.append(temp)
+            
+    return data
--- api/utility.py
+++ api/utility.py
@@ -10,7 +10,7 @@ def load_goods_data():
         header = next(csv_reader)
         for row in csv_reader:
             temp = {}
-            temp['goods_desc'] = row[0]
+            temp['title'] = row[0]
             temp['uom'] = row[1]
             temp['price'] = row[2]
             temp['currency'] = row[3]
@@ -28,6 +28,9 @@ def get_currency_rate():
         for row in csv_reader:
             data[row[0]] = float(row[1])
     return data
+
+def store_output(query, product_data):
+    pass
 
 
 def calculate_over_under(currency_rate, org_price, org_currency, price, currency):
@@ -49,19 +52,19 @@ def fuzzy_extract(choices, currency_rate, query, price, uom, currency, algo, thr
     results = []
     for item in choices:
         if algo == "ratio":
-            if fuzz.ratio(query, item['goods_desc']) >= threshold:
+            if fuzz.ratio(query, item['title']) >= threshold:
                 item["Over/Under"] = calculate_over_under(currency_rate, price, currency, float(item["price"]), item["currency"])
                 results.append(item)
         elif algo == "partial_ratio":
-            if fuzz.partial_ratio(query, item['goods_desc']) >= threshold:
+            if fuzz.partial_ratio(query, item['title']) >= threshold:
                 item["Over/Under"] = calculate_over_under(currency_rate, price, currency, float(item["price"]), item["currency"])
                 results.append(item)
         elif algo == "token_sort_ratio":
-            if fuzz.token_sort_ratio(query, item['goods_desc']) >= threshold:
+            if fuzz.token_sort_ratio(query, item['title']) >= threshold:
                 item["Over/Under"] = calculate_over_under(currency_rate, price, currency, float(item["price"]), item["currency"])
                 results.append(item)
         else:
-            if fuzz.token_set_ratio(query, item['goods_desc']) >= threshold:
+            if fuzz.token_set_ratio(query, item['title']) >= threshold:
                 item["Over/Under"] = calculate_over_under(currency_rate, price, currency, float(item["price"]), item["currency"])
                 results.append(item)
         
