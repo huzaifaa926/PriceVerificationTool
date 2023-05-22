c9bb4cc54cf38189e0fcc12e0f644ef69bdec5bb
Huzaifa Abid <huzaifaa926@gmail.com>
2023-05-22 16:55:33 +0500

    scraping partially completed, started working on v3

--- api/main.py
+++ api/main.py
@@ -3,9 +3,24 @@ from thefuzz import fuzz, process
 
 from models import SearchQuery
 from utility import *
+from scrap_alibaba_selenium import *
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -18,5 +33,23 @@ def search(query: SearchQuery):
     goods_data = load_goods_data()
     currency_rate = get_currency_rate()
     output = fuzzy_extract(goods_data, currency_rate, query.goods_desc, query.price,
+                           query.uom, query.currency, query.algo, query.threshold)
+    return {"output": output}
+
+
+@app.post("/pvt/api/v2/search")
+def search(query: SearchQuery):
+    product_data = scrap(query.goods_desc)
+    currency_rate = get_currency_rate()
+    output = fuzzy_extract(product_data, currency_rate, query.goods_desc, query.price,
+                           query.uom, query.currency, query.algo, query.threshold)
+    return {"output": output}
+
+
+@app.post("/pvt/api/v3/search")
+def search(query: SearchQuery):
+    product_data = scrap(query.goods_desc)
+    currency_rate = get_currency_rate()
+    output = fuzzy_extract(product_data, currency_rate, query.goods_desc, query.price,
                            query.uom, query.currency, query.algo, query.threshold)
     return {"output": output}
